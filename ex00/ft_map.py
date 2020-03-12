e4db85e4662152f180dbac622673b6dc9828b595
Florian Muller <fmuller@student.42.fr>
2020-03-12 20:39:36 +0100

    doing ex00

---
+++ ex00/ft_map.py
@@ -0,0 +1,11 @@
+
+def ft_map(function_to_apply, list_of_inputs):
+    pass
+
+
+if __name__ == "__main__":
+    lst = [1, 2, 3, 4]
+    res = map(lambda x: str(x) + " is cool", lst)
+    # print(list(res))
+    print(list(res))
+    pass
