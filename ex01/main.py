b066617f51ffe6e2d232534546377c6e0e2de697
Florian Muller <fmuller@student.42.fr>
2020-03-16 18:11:19 +0100

    ex01 done

---
+++ ex01/main.py
@@ -0,0 +1,39 @@
+def what_are_the_vars(*args, **kwargs):
+    res = ObjectC()
+    dict_args = {f"var_{i}": val for i, val in enumerate(args)}
+    if set(kwargs) & set(dict_args):
+        return None
+
+    res.__dict__.update(dict_args)
+    res.__dict__.update(kwargs)
+    return res
+
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
